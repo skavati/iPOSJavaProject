5ef2d7bb9cb3d2ceeff4ae36f0691c10ff32ef2b
skava <sarath_kavati@pa.com.au>
2015-09-28 21:55:31 +1000

    testing Image capture

--- src/Base/BaseMethods.java
+++ src/Base/BaseMethods.java
@@ -328,7 +328,7 @@ public static  WebDriver driver ;
      }
 
      // To take screen shot of page
-     public  static void captureScreenShot(String obj) throws IOException {
+     public  static void captureScreenShot(WebDriver driver, String obj) throws IOException {
          File screenshotFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
          FileUtils.copyFile(screenshotFile,new File("C:\\Screenshots\\"+obj+"_"+GetTimeStampValue()+".png"));
    }
